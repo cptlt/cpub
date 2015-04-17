6a5a0036a72b2bcdbf8fde9fe9be936690c173d7
lyu <mqliutie@gmail.com>
2015-04-17 16:34:05 +0800

    后台页面构建

---
+++ Public/script/Index.index.js
@@ -0,0 +1,18 @@
+var h = document.getElementsByTagName("h4");
+for (var i=0; i < h.length; i++) {
+	
+	h[i].onclick = function(e) {
+		
+		var next = e.currentTarget.nextSibling
+		var bool = next.className.indexOf("hide");
+		if (bool > 0) {
+			
+			var show = next.className.replace(/hide/,"show");
+			next.className = show;
+		} else {
+			
+			var hide = next.className.replace(/show/,"hide");
+			next.className = hide;
+		}
+	}
+}
